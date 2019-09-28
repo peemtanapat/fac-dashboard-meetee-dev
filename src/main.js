67cd2e352467c387dbbc46d5221406565a707dcc
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-09-28 14:49:03 +0700

    init vue-native-websocket

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import server from './server'
+import VueNativeSock from 'vue-native-websocket'
+// import VueWebsocket from "vue-websocket";
 
 Vue.config.productionTip = false
+
+// Vue.use(VueWebsocket, "ws://localhost:9001");
+Vue.use(VueNativeSock, 'ws://localhost:9001', {
+  store: store,
+  // format: 'json'
+})
 
 new Vue({
   router,
