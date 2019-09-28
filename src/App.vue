6ddffa1e8b865ef471173d894fcd1dbe5a85a1dd
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.rh>
2019-09-28 14:51:20 +0700

    update router

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/fac-dashboard">Dashboard</router-link>
+      <router-link to="/fac-dashboard">Dashboard</router-link> | 
+      <router-link to="/controller">Controller</router-link>
     </div>
     <router-view/>
   </div>
